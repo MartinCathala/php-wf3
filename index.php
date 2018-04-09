e0e87776482c4b052577be16c9d8578f27126d9d
MartinCathala <martincathama@orange.fr>
2018-04-09 12:34:57 +0200

    Modif de lapage d'acceuil

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/include/init.php';
 <?php 
 include __DIR__ . '/layout/top.php';
 ?>
-<h1>Acceuil</h1>
+<h1>Page d'Acceuil</h1>
 <?php 
 include __DIR__ . '/layout/bottom.php';
 ?>
